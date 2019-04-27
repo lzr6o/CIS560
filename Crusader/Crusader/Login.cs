9425c32e16b7b47d5521b9fe4e4e819b8e3cf90c
Zhengrui Li <zhengrui@ksu.edu>
2019-04-27 16:19:14 -0500

    hw

--- Crusader/Crusader/Login.cs
+++ Crusader/Crusader/Login.cs
@@ -13,11 +13,7 @@ namespace Crusader
 {
     public partial class Login : Form
     {
-        String conn_string = "Data Source=mssql.cs.ksu.edu;Initial Catalog=zhengrui;Integrated Security=True";
-        SqlDataReader dr;
-        SqlCommand cmd;
-        SqlConnection con;
-        String q = "";
+        
 
         public Login()
         {
@@ -26,28 +22,7 @@ namespace Crusader
 
         private void btnSignIn_Click(object sender, EventArgs e)
         {
-            try
-            {
-                
-                con.Open();
-                q = "SELECT * FROM Crusader.[User] u WHERE u.UserName = " + txtUsername.Text;
-                cmd = new SqlCommand(q, con);
-                dr = cmd.ExecuteReader();
-                if (dr == null)
-                {
-                    MessageBox.Show("bad");
-                }
-                else
-                {
-                    MessageBox.Show("good");
-                    if(dr.Read())
-                        MessageBox.Show(dr[1].ToString());
-                }
-            }
-            catch(Exception ex)
-            {
-                MessageBox.Show(ex.ToString(), "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            }
+
         }
 
         private void btnSignUp_Click(object sender, EventArgs e)
@@ -57,7 +32,7 @@ namespace Crusader
 
         private void Login_Load(object sender, EventArgs e)
         {
-            con = new SqlConnection(conn_string);
+            
 
         }
     }
