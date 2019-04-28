4ac647c954db25f333631853467c35623316a37e
Zhengrui Li <zhengrui@ksu.edu>
2019-04-28 00:02:34 -0500

    caonima

--- Test.sql
+++ Test.sql
@@ -4,9 +4,16 @@ SELECT *
 FROM Crusader.[User] u
 ORDER BY u.UserId ASC;
 
-SELECT *
+SELECT DISTINCT n.Faction
 FROM Crusader.NPC n
 WHERE
 n.NPCName LIKE '%Remi%' AND
 n.Skill LIKE '%W%'
 ORDER BY n.NPCId ASC;
+
+SELECT *
+FROM Crusader.Hero h
+INNER JOIN Crusader.NPC n ON n.NPCName = h.SoulboundWeapon;
+
+SELECT DISTINCT h.Faction
+FROM Crusader.Hero h;
